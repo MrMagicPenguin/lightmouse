8b5dc9c2a1a83959d7c00578b1947b9d594cf37e
Noah Price <noah.price.237@gmail.com>
2025-04-02 16:54:00 -0400

    - Add function to write user configuration to Global Workspace configuration - Move activation to onStartupFinished

--- src/extension.ts
+++ src/extension.ts
@@ -3,15 +3,32 @@
 import { log } from 'console';
 import * as vscode from 'vscode';
 
+type colorConfiguration = {
+	operator: string,
+	color: string
+}
+
+type textMateRuleConfiguration = {
+	scope: string,
+	settings: {
+		foreground?: string,
+		fontStyle?: string
+	}
+}
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "lightmouse" is active!');
 	// Get configuration
-	const config = vscode.workspace.getConfiguration("Lightmouse");
-	const criticalKeywordConfig = config.get('criticalColors')
-	console.log("hello")
+	const config = vscode.workspace.getConfiguration("lightmouse");
+	const criticalKeywordConfig: colorConfiguration = config.get('criticalColors') || {operator:"", color:""}
+	// const editorConfig = vscode.workspace.getConfiguration('editor')
+	// const currentTokenColors = editorConfig.get('tokenColorCustomizations')
+	//console.log(JSON.stringify(criticalKeywordConfig))
+
+	updateTokenColors(criticalKeywordConfig)
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -28,17 +45,39 @@ export function activate(context: vscode.ExtensionContext) {
 // This method is called when your extension is deactivated
 export function deactivate() { }
 
-function updateTokenColors(config: vscode.WorkspaceConfiguration) {
+function updateTokenColors(keywordConfig: colorConfiguration) {
 	// Get our editor's existing settings, which we will add to.
-	const editorConfig = config.getConfiguration('editor')
-	const currentTokenColors = editorConfig.get('tokenColorCustomizations')
+	const editorConfig = vscode.workspace.getConfiguration('editor')
+	const currentTokenColors = editorConfig.get('tokenColorCustomizations') || {}
 
+	// Create Color rules based on the config we pass in.
+	//TODO Make this a generated array based on config.
 	const todoRules = [
 			{
-				"scope": "keyword.operator",
+				"scope": keywordConfig.operator,
 				"settings": {
-					"foreground": "#FF0000"
+					"foreground": keywordConfig.color
 				}
 			}
 		]
+	
+	const newTokenColors = {
+		// Start with all current TokenColorCustomization configurations,
+		// Remove any previous rules we may have created from this list as we will add out own.
+		...currentTokenColors,
+		"textMateRules": todoRules
+	}
+	console.log("Current: " + JSON.stringify(currentTokenColors))
+	console.log("New: " + JSON.stringify(newTokenColors))
+	
+
+	// Only update if our settings have changed
+	if (JSON.stringify(currentTokenColors) !== JSON.stringify(newTokenColors)){
+		console.log("Updating Editor Config...")
+		editorConfig.update('tokenColorCustomizations', newTokenColors, vscode.ConfigurationTarget.Global)
+	}
+
+	console.log
+	
+	
 }
